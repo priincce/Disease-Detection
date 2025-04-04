ab1d51a7182ceb1d2eca085f5f6232178101f8a7
princebeersingh <princebeersingh@gmail.com>
2025-04-05 02:26:23 +0530

    changes to model in 2025

--- Treatment.py
+++ Treatment.py
@@ -1,44 +1,50 @@
 import re
-from googlesearch import search
-import warnings
-warnings.filterwarnings("ignore")
 import requests
+import warnings
 from bs4 import BeautifulSoup
+from googlesearch import search
 
-# Take input a disease and return the content of wikipedia's infobox for that specific disease
+warnings.filterwarnings("ignore")
 
 def diseaseDetail(term):
-    diseases=[term]
-    ret=term+"\n"
+    diseases = [term]
+    result = term + "\n"
+
     for dis in diseases:
-        # search "disease wilipedia" on google 
-        query = dis+' wikipedia'
-        for sr in search(query,tld="co.in",stop=10,pause=0.5): 
-            # open wikipedia link
-            match=re.search(r'wikipedia',sr)
-            filled = 0
-            if match:
-                wiki = requests.get(sr,verify=False)
-                soup = BeautifulSoup(wiki.content, 'html5lib')
-                # Fetch HTML code for 'infobox'
-                info_table = soup.find("table", {"class":"infobox"})
-                if info_table is not None:
-                    # Preprocess contents of infobox
-                    for row in info_table.find_all("tr"):
-                        data=row.find("th",{"scope":"row"})
-                        if data is not None:
-                            symptom=str(row.find("td"))
-                            symptom = symptom.replace('.','')
-                            symptom = symptom.replace(';',',')
-                            symptom = symptom.replace('<b>','<b> \n')
-                            symptom=re.sub(r'<a.*?>','',symptom) # Remove hyperlink
-                            symptom=re.sub(r'</a>','',symptom) # Remove hyperlink
-                            symptom=re.sub(r'<[^<]+?>',' ',symptom) # All the tags
-                            symptom=re.sub(r'\[.*\]','',symptom) # Remove citation text                     
-                            symptom=symptom.replace("&gt",">")
-                            ret+=data.get_text()+" - "+symptom+"\n"
-#                            print(data.get_text(),"-",symptom)
-                            filled = 1
-                if filled:
-                    break
-    return ret
+        query = dis + ' wikipedia'
+
+        for sr in search(query, tld="co.in", num_results=10, pause=0.5):
+            if "wikipedia" in sr:
+                try:
+                    response = requests.get(sr, verify=False)
+                    soup = BeautifulSoup(response.content, 'html5lib')
+
+                    info_table = soup.find("table", {"class": "infobox"})
+                    if info_table is not None:
+                        filled = False
+                        for row in info_table.find_all("tr"):
+                            data = row.find("th", {"scope": "row"})
+                            if data:
+                                value = str(row.find("td"))
+
+                                # Clean the HTML content
+                                value = value.replace('.', '')
+                                value = value.replace(';', ',')
+                                value = value.replace('<b>', '<b> \n')
+                                value = re.sub(r'<a.*?>', '', value)
+                                value = re.sub(r'</a>', '', value)
+                                value = re.sub(r'<[^<]+?>', ' ', value)
+                                value = re.sub(r'\[.*?\]', '', value)
+                                value = value.replace("&gt", ">")
+
+                                # Append to result
+                                result += data.get_text().strip() + " - " + value.strip() + "\n"
+                                filled = True
+
+                        if filled:
+                            break
+                except Exception as e:
+                    result += f"Error fetching data from {sr}: {e}\n"
+                    continue
+
+    return result
